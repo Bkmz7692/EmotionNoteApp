3b0afec1022aa8d082a1daf6126f38ffcc82531b
Ilya Korotaev <korotaevilv@icloud.com>
2024-02-24 17:22:51 +0400

    Initial commit

---
+++ app/src/main/java/com/bkmzdev/dreamjournal/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.bkmzdev.dreamjournal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bkmzdev.dreamjournal.R.layout.fragment_home
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(fragment_home, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/bkmzdev/dreamjournal/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.bkmzdev.dreamjournal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        replaceFragment(HomeFragment.newInstance(),false)
+    }
+
+    fun replaceFragment(fragment:Fragment, istransition:Boolean){
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+
+        if (istransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.add(R.id.frame_Layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+    }
+
+
+
+}
